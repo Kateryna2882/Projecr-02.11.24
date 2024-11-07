69dd7ffe595e3f04b0cdc4a32206d757e7a1e380
Kateryna2882 <govoryhina82@gmail.com>
2024-11-07 19:48:29 +0200

    Add Arrays class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        float[][] floatTwoD;
+        floatTwoD = new float[4][10];
+
+        char[][] ticTacToe = {{'X', 'O', 'O'}, {'O', 'X', ' '}, {'X', ' ', 'X'}};
+
+        ticTacToe = new char[][] {{'O', 'X', 'X'}, {'X', 'O', ' '}, {'O', ' ', 'O'}};
+
     }
 }
