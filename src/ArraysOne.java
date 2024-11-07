c42d2991f8d662fa68769fe26351019732c8c13f
Kateryna2882 <govoryhina82@gmail.com>
2024-11-07 20:18:42 +0200

    Add ArraysOne class

---
+++ src/ArraysOne.java
@@ -0,0 +1,12 @@
+public class ArraysOne {
+    public static void main(String[] args) {
+        int tableSize = 10;
+        for(int i = 0; i < 10; i++){
+            for(int j = 10; j > 1; j--){
+                System.out.print(i * j);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
