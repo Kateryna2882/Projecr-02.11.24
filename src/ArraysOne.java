3d5b4e474d82224b3539304c87eb13b8b20807e4
Kateryna2882 <govoryhina82@gmail.com>
2024-11-07 20:44:02 +0200

    Add fields and methods

--- src/ArraysOne.java
+++ src/ArraysOne.java
@@ -1,8 +1,8 @@
 public class ArraysOne {
     public static void main(String[] args) {
         int tableSize = 10;
-        for(int i = 0; i < 10; i++){
-            for(int j = 10; j > 1; j--){
+        for(int i = 1; i <= 10; i++){
+            for(int j = 1; j <= 10; j++){
                 System.out.print(i * j);
                 System.out.print(" ");
             }
